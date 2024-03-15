d5eb0ea50800e137a47876aafc546d9ad57c18de
Reza-Riswandha <rezariswan29@gmail.com>
2024-03-15 21:14:24 +0700

    Add form makanan on makanan screen

--- lib/screen/Makanan_Screen.dart
+++ lib/screen/Makanan_Screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ucp1/widget/form_makanan.dart';
 import 'package:ucp1/widget/header_makanan.dart';
 
 class MakananScreen extends StatefulWidget {
@@ -7,11 +8,15 @@ class MakananScreen extends StatefulWidget {
   final String nama;
   final String telepon;
 
+  
+
   @override
   State<MakananScreen> createState() => _MakananScreenState();
 }
 
 class _MakananScreenState extends State<MakananScreen> {
+    var desrt = TextEditingController();
+    var formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +26,8 @@ class _MakananScreenState extends State<MakananScreen> {
           children: [
             HeaderMakanan(),
             Text('Nama : ${widget.nama}'),
-            Text('Nomor Telepon : ${widget.telepon}')
+            Text('Nomor Telepon : ${widget.telepon}'),
+            FormMakanan(etDesert: desrt, formkey: formkey,),
           ],
         ),
       ),
--- lib/widget/form_makanan.dart
+++ lib/widget/form_makanan.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 class FormMakanan extends StatefulWidget {
-  const FormMakanan({super.key});
+  const FormMakanan({super.key, required this.formkey, required this.etDesert});
+
+  final GlobalKey<FormState> formkey;
+  final TextEditingController etDesert;
 
   @override
   State<FormMakanan> createState() => _FormMakananState();
@@ -10,6 +13,68 @@ class FormMakanan extends StatefulWidget {
 class _FormMakananState extends State<FormMakanan> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Form(
+        key: widget.formkey,
+        child: Column(
+          children: [
+            TextFormField(
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                labelText: 'Makanan',
+                hintText: 'Masukkan Nama Makanan',
+                prefixIcon: const Icon(Icons.fastfood),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Tidak Boleh Kosong";
+                }
+                return null;
+              },
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                labelText: 'Minuman',
+                hintText: 'Masukkan Nama Minuman',
+                prefixIcon: const Icon(Icons.local_drink),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Tidak Boleh Kosong";
+                }
+                return null;
+              },
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              controller: widget.etDesert,
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                labelText: 'Dessert',
+                hintText: 'Masukkan Nama Dessert',
+                prefixIcon: const Icon(Icons.bakery_dining),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Tidak Boleh Kosong";
+                }
+                return null;
+              },
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+            ),
+          ],
+        ));
   }
 }
